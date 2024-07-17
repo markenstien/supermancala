6113308208750622d8ca7637d585c96299d6e45f
Mark Angelo Gonzales <chromaticsoftwares@gmail.com>
2024-07-18 02:48:02 +0800

    catalog page

--- app/controllers/GameController.php
+++ app/controllers/GameController.php
@@ -18,8 +18,8 @@
 		 */
 		public function index() {
 			// _requireAuth();
-			$this->data['boardLength'] = 6;
-			return $this->view('game/index', $this->data);
+
+			return $this->catalog();
 		}
 
 		/**
