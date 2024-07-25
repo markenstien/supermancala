b9338c3382ec34f7f4df0b8a2b0e577a68d5785d
Mark Angelo Gonzales <chromaticsoftwares@gmail.com>
2024-07-26 05:01:13 +0800

    apply fix

--- app/config/constants.php
+++ app/config/constants.php
@@ -107,7 +107,7 @@
 
         [
             'id' => '6',
-            'code'=> 'Snake',
+            'code'=> 'snake',
             'title' => 'Snake',
             'description' => "Snake is a genre of action video games where the player maneuvers the end of a growing line, often themed as a snake. 
             The player must keep the snake from colliding with both other obstacles and itself, which gets harder as the snake lengthens.",
