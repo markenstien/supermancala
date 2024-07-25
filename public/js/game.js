98c39f2e38d3bbcd418530faed92714a8aa26afa
Mark Angelo Gonzales <chromaticsoftwares@gmail.com>
2024-07-26 05:02:45 +0800

    add js fix

--- public/js/game.js
+++ public/js/game.js
@@ -1,11 +1,3 @@
-const GAMES = {
-    horseracing : 1,
-    tetris : 2,
-    lucky9 : 3,
-    mancala : 4,
-    snake : 6,
-    hoops : 5,
-}
 function gameScoreSave(gameId, score) {
     $.ajax({
         url : getURL('api/GameScorer/addScore'),
